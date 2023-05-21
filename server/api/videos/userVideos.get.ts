0a10ad1161ae3f2360cc882e01d3c3e95f9bab19
Dimitri Leduc <98540007+dimitrileduc@users.noreply.github.com>
2023-05-21 09:28:50 +0200

    correct endpoint

--- server/api/videos/userVideos.get.ts
+++ server/api/videos/userVideos.get.ts
@@ -1,43 +1,32 @@
 import { PrismaClient } from "@prisma/client";
-import protectRoute from "~~/server/utils/protectRoute";
 
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-  console.log("user  videos get endpoint called ");
-
-  protectRoute(event);
-
-  console.log("user videos get - ok user logged ");
-
   const userEmail = event.context.user?.email;
 
+  // Get the all videos based on the `videoUpdated` field
+  const videos = await prisma.video.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  // user not logged in , return placeholder images and video infos without link
   if (!userEmail) {
     return null;
-  }
-
-  // Get all VideoPurchase records for the user
-  const videoPurchases = await prisma.videoPurchase.findMany({
-    where: {
-      userEmail,
-    },
-  });
-
-  if (!videoPurchases.length) {
-    return null;
-  }
-
-  // Map the videoIds from the VideoPurchase records
-  const videoIds = videoPurchases.map((vp) => vp.videoId);
-
-  // Get all videos that correspond to the videoIds
-  const videos = await prisma.video.findMany({
-    where: {
-      id: {
-        in: videoIds,
+  } else {
+    // Check if the user has purchased each video
+    const videoPurchases = await prisma.videoPurchase.findMany({
+      where: {
+        userEmail,
+        videoId: { in: videos.map((video) => video.id) },
       },
-    },
-  });
+      select: {
+        videoId: true,
+      },
+    });
 
-  return videos;
+    return videoPurchases;
+  }
 });
