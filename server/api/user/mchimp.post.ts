8c66c34a96ab84e69000e86f4f0a479d27e7e889
Dimitri Leduc <98540007+dimitrileduc@users.noreply.github.com>
2023-06-01 19:33:46 +0200

    mailchimp registered user

--- server/api/user/mchimp.post.ts
+++ server/api/user/mchimp.post.ts
@@ -4,17 +4,20 @@ export default async function defineEventHandler(event: any) {
   const { email, username } = await readBody(event);
 
   MailchimpMarketing.setConfig({
-    apiKey: "a8fa444f14add11e519c69c9af71afc9-us21",
+    apiKey: "8c3b129557bce89151727bbcf3e892a3-us21",
     server: "us21",
   });
 
   const run = async () => {
     try {
       const response = await MailchimpMarketing.lists.addListMember(
-        "b4b2f71909",
+        "cd76cd6995",
         {
           email_address: email,
           status: "subscribed",
+          merge_fields: {
+            TYPE: "Registered", // Set the value based on the user's registration status
+          },
         }
       );
       console.log(response);
@@ -25,6 +28,13 @@ export default async function defineEventHandler(event: any) {
     }
   };
 
+  const runListsMergedFields = async () => {
+    const response = await MailchimpMarketing.lists.getListMergeFields(
+      "cd76cd6995"
+    );
+    console.log(response);
+  };
+  runListsMergedFields;
   const response = run();
   return response;
 }
