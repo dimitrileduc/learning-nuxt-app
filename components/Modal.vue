984c6318aabe6cf3be866e09971c6c348c1f0b86
Dimitri Leduc <98540007+dimitrileduc@users.noreply.github.com>
2023-06-03 16:43:37 +0200

    icon close on modal

--- components/Modal.vue
+++ components/Modal.vue
@@ -4,7 +4,12 @@
       class="modal backdrop-blur-l backdrop-brightness-50 w-100vw h-100vh flex flex-col justify-center items-center fixed inset-0 z-50"
       @click.self="$emit('close')"
     >
-      <slot />
+      <div class="modal-content">
+        <slot />
+        <div class="close-arrow" @click="$emit('close')">
+          <Icon size="24px" name="mingcute:close-fill" color="#0D4247" />
+        </div>
+      </div>
     </div>
   </Teleport>
 </template>
@@ -28,5 +33,16 @@ onMounted(() => {
 .modal {
   z-index: 100;
   background-color: rgb(16, 75, 81, 0.7);
+}
+
+.modal-content {
+  position: relative;
+}
+
+.close-arrow {
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  cursor: pointer;
 }
 </style>
